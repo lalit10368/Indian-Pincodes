ade88c976939e894762ce5d2f5e4de1c03e411d2
Lalit Kumar <Lalit.Kumar@paysafe.com>
2021-09-07 09:11:55 +0530

    renamed app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var state_district_list = require('./data/district_list.json')
 var states = Object.keys(state_district_list)
 let fileName = "./data/state_district_url.txt"
 let area_list_file = "./data/area_list.txt"
+fs.rmSync('./data/pincodes_raw.txt', {'force' : true})
 //fs.rmSync('./data/pincodes.txt', {'force' : true})
 //fs.rmSync(fileName, {'force' : true})
 //fs.rmSync('area_list.txt', {'force' : true})
@@ -36,7 +37,7 @@ const get_all_urls = () => {
             fetch_json(url)
                 .then(areas => {
                     areas = areas.map(area => { return area.url })
-                    area = areas[0]
+                    area = areas[Math.floor(Math.random()*areas.length)]
                     fs.appendFileSync(area_list_file, `${line}${area}` + os.EOL)
                 })
                 .catch(err => { console.log(err)})
@@ -45,28 +46,28 @@ const get_all_urls = () => {
 }
 
 const get_pincode = async () => {
-    const processed = fs.readFileSync('./data/pincodes.txt').toString().split(os.EOL)
-        .map(prefix => {
-            return prefix.replace(new RegExp("/[0-9]+"), "")
-        })
+    //const processed = fs.readFileSync('./data/pincodes.txt').toString().split(os.EOL)
+    //    .map(prefix => {
+    //        return prefix.replace(new RegExp("/[0-9]+"), "")
+    //    })
     //console.log(processed)
     const content = fs.readFileSync(area_list_file).toString().split(os.EOL)
-        .filter(prefix => {
-            return !processed.includes(prefix) 
-        })
     return Promise.map(content, prefix => {
         let url = `https://www.indiatvnews.com/pincode${prefix}`;
         return fetch_html(url)
             .then(html => {
                 const $ = cheerio.load(html);
-                const meta = $('meta[name="description"]').attr('content')
-                fs.appendFileSync('./data/pincodes.log', `${prefix}|${meta}|${os.EOL}`)
-                return meta
+                //const meta = $('meta[name="description"]').attr('content')
+                return $("body > div:nth-child(5) > div.row.two_column > div.lhs.mb30 > div.lhsBox.maincontent.article.pull-right > div.pin-code-box > div:nth-child(5) > table > tbody").text()
             })
             .then(content => {
-                let pincode = content.match('[0-9]+')[0]
-                let data = `${prefix}/${pincode}`
-                fs.appendFileSync('./data/pincodes.txt', data + os.EOL)
+                //let pincode = content.match('[0-9]+')[0]
+                //let data = `${prefix}/${pincode}`
+                let rows = content.split("\n").map(r => r.trim())
+                rows.shift()
+                rows.forEach(row => {
+                    fs.appendFileSync('./data/pincodes_raw.txt', row + os.EOL)
+                })
                 return data
             })
             .catch(err => {
@@ -80,11 +81,8 @@ const get_pincode = async () => {
 }
 // $("body > div:nth-child(5) > div.row.two_column > div.lhs.mb30 > div.lhsBox.maincontent.article.pull-right > div.pin-code-box > div:nth-child(5) > table > tbody")
 
-get_all_urls().then(_ => {
-    get_pincode()
-}).catch(err => {
-    console.log(err)
-})
+//get_all_urls()
+get_pincode()
 
 // URL for post offices : https://www.indiatvnews.com/pincode/ajaxpostofficeload/andhra-pradesh/ananthapur
 // URL for html content : https://www.indiatvnews.com/pincode/andhra-pradesh/ananthapur/zilladakunta 
